62ffc2012fafce8f5a104607c8064d61186fd6b8
Pablo A Nistal <niskeletor@gmail.com>
2023-05-17 14:26:36 +0200

    Creadas clase DAO para Usuarios WEB

--- src/main/java/modeloUserWeb/AdminWeb.java
+++ src/main/java/modeloUserWeb/AdminWeb.java
@@ -2,6 +2,23 @@ package modeloUserWeb;
 
 public class AdminWeb extends UserWeb{
 
+	public AdminWeb() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public AdminWeb(String nombre, String apellidos, String nombreUsuario, String passw, String email, String foto,
+			String departamento, String empresa, int id) {
+		super(nombre, apellidos, nombreUsuario, passw, email, foto, departamento, empresa, id);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AdminWeb(String nombre, String apellidos, String nombreUsuario, String passw, String email,
+			String departamento, String empresa) {
+		super(nombre, apellidos, nombreUsuario, passw, email, departamento, empresa);
+		// TODO Auto-generated constructor stub
+	}
+
 	
 	
 
