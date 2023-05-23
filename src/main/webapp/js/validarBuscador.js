f7d92202fd6b5c85366ddf58f791e31d00c64fc4
Pablo A Nistal <niskeletor@gmail.com>
2023-05-23 15:06:21 +0200

    Corregido script Buscador indicaciones visuales en campos errones, validar daot, permite algún campo vacío

--- src/main/webapp/js/validarBuscador.js
+++ src/main/webapp/js/validarBuscador.js
@@ -1,4 +1,8 @@
 function validateForm() {
+
+    //Evitar que la pagina se recargue
+    event.preventDefault();
+
     // Captura de los valores de los campos del formulario
     var nombre = document.forms["userForm"]["nombre"].value;
     var apellido = document.forms["userForm"]["surname"].value;
@@ -19,8 +23,14 @@ function validateForm() {
     var errores = 0;
     var mensaje = "";
 
+    //comprobar que al menos un campo se haya rellenado 
+    if (nombre === "" && apellido === "" && usuario === "" && departamento === "0" && empresa === "0" && !administrador) {
+        errores++;
+        mensaje += "Debe ingresar al menos un campo para realizar la búsqueda.\n";
+    } else {
+
     // Validación de los campos
-    if (!nombreRegex.test(nombre)) {
+    if (!nombreRegex.test(nombre) && ( !(nombre === "") )) {
         errores++;
         mensaje += "El nombre solo puede contener caracteres alfabéticos y tener hasta 25 caracteres.\n";
         document.getElementById("nombreIcon").className = "fas fa-times-circle ";
@@ -28,7 +38,7 @@ function validateForm() {
         document.getElementById("nombreIcon").className = "fas fa-check-circle";
     }
 
-    if (!apellidoRegex.test(apellido)) {
+    if (!apellidoRegex.test(apellido) && ( !(apellido === "") )) {
         errores++;
         mensaje += "Los apellidos solo pueden contener caracteres alfabéticos y tener hasta 45 caracteres.\n";
         document.getElementById("apellidoIcon").className = "fas fa-times-circle ";
@@ -36,9 +46,12 @@ function validateForm() {
         document.getElementById("apellidoIcon").className = "fas fa-check-circle";
     }
 
-    if (!usuarioRegex.test(usuario)) {
+    if (!usuarioRegex.test(usuario) && ( !(usuario === "") ) ) {
         errores++;
         mensaje += "El nombre de usuario solo puede contener caracteres alfabéticos y tener hasta 10 caracteres.\n";
+        document.getElementById("userIcon").className = "fas fa-times-circle ";
+    } else {
+        document.getElementById("userIcon").className = "fas fa-check-circle";
     }
 
     if (!depEmpRegex.test(departamento)) {
@@ -56,6 +69,7 @@ function validateForm() {
         errores++;
         mensaje += "El valor de administrador es inválido.\n";
     }
+}
 
     if (errores > 0) {
         alert(mensaje);
