5e7b6b68da1d5f21f6d528ec13e0441d3dfa3927
Pablo A Nistal <niskeletor@gmail.com>
2023-06-01 19:53:42 +0200

    HA OCURRIDO ERROR EN SERVLETS

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"dao"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controlador"},{"l":"dao"},{"l":"login"},{"l":"modelo"},{"l":"modeloListador"},{"l":"modeloUserWeb"}];updateSearchResults();
